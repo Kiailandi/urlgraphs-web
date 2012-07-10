580657a4a82467afce66026b1831a0be50436516
TommasoMontefusco <Jock3r@hotmail.it>
2012-07-10 16:35:12 +0200

    Settings transmission and filtering implemented

--- urlgraphs-Server/server.py
+++ urlgraphs-Server/server.py
@@ -1,6 +1,6 @@
 __author__ = 'tommaso'
 
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, url_for, jsonify, request
 
 app = Flask(__name__)
 
@@ -32,11 +32,67 @@ def page_not_found(error):
 @app.route('/submit/', methods=['POST'])
 def submit():
     import json
+#   import pdb; pdb.set_trace()
     db = get_redis()
-    import requests
 
-#    urls = ['http://www.google.com', 'www.facebook.com']
+    tmp = request.form
 
+#   cosa schifosa da modificare al piu' presto
+    data = dict()
+
+    data['profondita'] = tmp['profondita']
+    data['timeout'] = tmp['timeout']
+    data ['Turisti_per_caso'] = tmp['Turisti_per_caso']
+    data ['Vbulletin_Topic'] = tmp['Vbulletin_Topic']
+    data ['Vbulletin_Section'] = tmp['Vbulletin_Section']
+    data ['Yahoo_Answer'] = tmp['Yahoo_Answer']
+    data ['All_Ahref'] = tmp['All_Ahref']
+    data ['DiffBot'] = tmp['DiffBot']
+
+#   debug
+    print "Data recived:"
+    print data
+
+    urls = tmp['input'].split('\n')
+#   debug
+    print urls
+
+#   pulisce da eventuali componenti vuote
+    urls = [url for url in urls if url]
+#   semi-schifezza come sopra
+    data['url'] = urls
+#   debug
+    print urls
+
+    try:
+        if int(data['profindita']) > 5:
+            data['profondita'] = 3
+    except:
+        data['profondita'] = 3
+    finally:
+#       dubug
+        print data['profondita']
+
+    try:
+        if int(data['timeout']) > 120:
+            data['timeout'] = 30
+    except:
+        data['timeout'] = 30.
+    finally:
+#       dubug
+        print data['timeout']
+#   da migliorare, sintassi dispersiva e valori come stringhe e non booleani
+    if  data ['Turisti_per_caso'] == 'false' and   \
+        data ['Vbulletin_Topic'] == 'false' and    \
+        data ['Vbulletin_Section'] == 'false' and  \
+        data ['Yahoo_Answer'] == 'false' and       \
+        data ['All_Ahref'] == 'false' and          \
+        data ['DiffBot'] == 'false':
+        data ['DiffBot'] = 'true'
+#       debug
+        print data ['DiffBot']
+
+#   urls = ['http://www.google.com', 'www.facebook.com']
 #    data = dict(
 #      timeout=30.,
 #      urls=urls,
--- urlgraphs-Server/static/input.js
+++ urlgraphs-Server/static/input.js
@@ -2,8 +2,8 @@ function Input(){
 return document.UrlProfondita.profondita.value + "\r\n" 
      + document.UrlTimeout.timeout.value       + "\r\n"
      + document.UrlInput.input.value           + "\r\n"
-     + document.UrlOutput.output.value 	       + "\r\n"
-     + document.UrlFake.fake.value             + "\r\n"
-     + document.UrlShots.shots.value           + "\r\n"
+     //+ document.UrlOutput.output.value         + "\r\n"
+     //+ document.UrlFake.fake.value             + "\r\n"
+     //+ document.UrlShots.shots.value           + "\r\n"
      + "Sended"
 }
